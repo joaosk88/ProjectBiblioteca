fdce425405402a5d1052bf425c442017380b6e58
rodrigo-br <rodrigoab123@gmail.com>
2024-08-19 14:52:43 -0300

    novos testes

--- TestsXUnit/TestesLivro.cs
+++ TestsXUnit/TestesLivro.cs
@@ -1,11 +1,43 @@
+using ProjectBiblioteca.Models;
+
 namespace TestsXUnit
 {
     public class TestesLivro
     {
         [Fact]
-        public void Test1()
+        public void Teste_Instancia_De_Livro_Criando_Corretamente()
         {
+            Livro livro = new Livro
+            {
+                Titulo = "O Senhor dos Anéis",
+                Autor = "J. R. R. Tolkien",
+                Editora = "HarperCollins",
+                Genero = "Fantasia",
+            };
 
+            Assert.NotNull(livro);
+        }
+
+        [Fact]
+        public void Teste_Valores_De_Livro_Conforme_Construcao()
+        {
+            string titulo = "O Senhor dos Anéis";
+            string autor = "J. R. R. Tolkien";
+            string editora = "HarperCollins";
+            string genero = "Fantasia";
+
+            Livro livro = new Livro
+            {
+                Titulo = titulo,
+                Autor = autor,
+                Editora = editora,
+                Genero = genero,
+            };
+
+            Assert.Equal(titulo, livro.Titulo);
+            Assert.Equal(autor, livro.Autor);
+            Assert.Equal(editora, livro.Editora);
+            Assert.Equal(genero, livro.Genero);
         }
     }
 }
---
+++ TestsXUnit/TestesUsuario.cs
@@ -0,0 +1,31 @@
+﻿using ProjectBiblioteca.Models;
+
+namespace TestsXUnit
+{
+    public class TestesUsuario
+    {
+        [Fact]
+        public void Teste_Instancia_De_Usuario_Criando_Corretamente()
+        {
+            Usuario usuario = new Usuario
+            {
+                Nome = "Claudinei",
+            };
+
+            Assert.NotNull(usuario);
+        }
+
+        [Fact]
+        public void Teste_Valores_De_Usuario_Conforme_Construcao()
+        {
+            string nome = "Claudinei";
+
+            Usuario usuario = new Usuario
+            {
+                Nome = nome,
+            };
+
+            Assert.Equal(nome, usuario.Nome);
+        }
+    }
+}
