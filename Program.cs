c31adc111f4c0447ec72526340e33f61a6a45711
JHONDKA <joaovivask8@gmail.com>
2024-08-19 11:43:34 -0300

    <criação de classes específicas para organizar os dados de um livro e utilizar essas classes no código principal para manipulação e exibição.>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    using System;
+using MyLibraryProject.Models;
+
+namespace MyLibraryProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ProjectBiblioteca projectBiblioteca = new ProjectBiblioteca
+            {
+                Titulo = "O Senhor dos Anéis",
+                Genero = "Ficção",
+                Editor = "Imfe",
+                Autor = "Tolkien"
+            };
+
+            Console.WriteLine(projectBiblioteca);
+        }
+    }
+}
+
+        
+}
